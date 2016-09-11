2300c2c645f54336681f84f764db32800414539b
Mike Duffey <mpduffey@gmail.com>
2016-09-11 20:06:56 +0000

    WFT

---
+++ single-page-scrolling.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:   'single-page-scrolling',
+    template:   `
+      <modal></modal>
+      <nav2-menu></nav2-menu>
+      <intro-section></intro-section>
+      <about-section></about-section>
+      <event-calendar></event-calendar>
+    `
+})
+
+export class SinglePageScrolling {
+	
+}
