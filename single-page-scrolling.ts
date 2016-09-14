7363d11165f695dae652904649859e6303f11cac
Mike Duffey <mpduffey@gmail.com>
2016-09-14 04:26:01 +0000

    AddedContact

--- single-page-scrolling.ts
+++ single-page-scrolling.ts
@@ -8,7 +8,17 @@ import {Component} from '@angular/core';
 		<intro-section></intro-section>
 		<about-section></about-section>
 		<event-calendar></event-calendar>
-		<section id="calendar" class="section-padding bg-image overlay-dark dark-bg text-center" data-stellar-background-ratio="0.5" data-background-img="img/full/33.jpg">
+		<section id="contact" class="section-padding bg-image overlay-dark dark-bg text-center" data-stellar-background-ratio="0.5" data-background-img="img/full/33.jpg">
+			<div class="container">
+				<div class="row">
+					<div>
+						<p>Contact Wisconsin Women for Trump</p>
+						<p><a href="mailto:wwft2016@gmail.com" target="_top">wwft2016@gmail.com</a><p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="footer" class="section-padding bg-image overlay-dark dark-bg text-center" data-stellar-background-ratio="0.5" data-background-img="img/full/33.jpg">
 			<div class="container">
 				<div class="row">
 					<div class="footer">Paid For By Donald J. Trump for President</div>
@@ -18,6 +28,7 @@ import {Component} from '@angular/core';
 	`,
 	styles:			[`
 		.footer {margin: 10px auto; border: white 3px solid; padding: 20px; font-weight: 600; color: white; text-align: center; font-size: 16px;}
+		#contact a:hover {color: white;}
 	`]
 })
 
